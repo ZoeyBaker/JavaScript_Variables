226ad189c3ebb9215715b35b1b1fb020276d593d
Zoey Baker <zoeybaker625@gmail.com>
2023-02-21 07:37:30 -0600

    initial commit

--- app.js
+++ app.js
@@ -5,6 +5,23 @@ console.log(
 
 // Exercise 1
 
+const firstName = "Zoey"
+
+let lastName = "Lisa"
+
+var age = "30"
+
 // Exercise 2
 
+let fullName = firstName + "" + lastName;
+console.log(fullName);
+
+fullName = '$[firstName] ${lastName]';
+
 // Exercise 3
+
+let city = "Nashville";
+let passTime = "Gaming";
+let myStory = 'Hi! I am ${fullName}. I live in ${city}. I enjoy ${passTime} and coding';
+console.log(myStory);
+
